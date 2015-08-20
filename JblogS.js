b8adb98daa0055e936c32c07c1e84141728bb8e4
_hashtag <HashtagMC@users.noreply.github.com>
2015-08-20 21:15:23 +0200

    Fx
    Fixed an error caused by me working with old versions of this file.

--- JblogS.js
+++ JblogS.js
@@ -1,18 +1,19 @@
 var converter = new Markdown.Converter();
 
-var blog = "";
-
-for (i = 0; i < blogEntries.length; i++) {
-	$("#blog").load(blogEntries[i], function(data) {
-		var text = converter.makeHtml(data);
-		blog += text;
-		blog = metadata(blog);
-		blog += "<hr>";
-		document.getElementById("blog").innerHTML=blog;
-	});
+for (entry of blogEntries) {
+    var id = "entry-" + entry;
+    var elem = $("<p id='" + id + "'/>");
+    
+    $("#blog").append(elem);
+    
+    elem.load(entry, function(data) {
+	var text = converter.makeHtml(data);
+	var text = metadata(text);
+        this.innerHTML = text;
+    });
 }
 
 function metadata(blogpost) {
-	blogpost = blogpost.replace(/\[metadata=(.*)[;](.*)\]/, "<p>"+authorstring+" "+'$1'+" "+datestring+" "+'$2'+"</p>", "gi");
+	blogpost = blogpost.replace(/\[metadata=(.*)[;](.*)\]/, "<p class="jblogs-metadata">"+authorstring+" "+'$1'+" "+datestring+" "+'$2'+"</p>", "gi");
 	return blogpost;
 }

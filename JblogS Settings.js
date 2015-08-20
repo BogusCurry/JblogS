63cb7f646687702bf8193684b12e031d1e189837
_hashtag <HashtagMC@users.noreply.github.com>
2015-08-20 21:07:12 +0200

    Settings file
    Link this file before loading JblogS.js e.g. in the head of the .html.

---
+++ JblogS Settings.js
@@ -0,0 +1,8 @@
+//Metadata
+//Example: Written by HashtagMC at 01.01.1980
+//--------authorstring----------datestring-------------
+var authorstring = "Written by";
+//default: 'Written by'
+
+var datestring = "at";
+//default: 'at'
